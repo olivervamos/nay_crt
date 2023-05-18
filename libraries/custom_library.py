6bcb3dc09e0855f6b4d76cb3559366c547dc2e4c
Oliver Vamos <oliver.vamos@sanae.sk>
2023-05-18 16:55:03 +0200

    modified:   libraries/__pycache__/custom_library.cpython-311.pyc 	modified:   libraries/custom_library.py

--- libraries/custom_library.py
+++ libraries/custom_library.py
@@ -1,6 +1,5 @@
 from QWeb.keywords.element import get_element_count
 from QWeb.keywords.text import get_text
-from robot.libraries.String import String
 import logging
 
 
@@ -8,14 +7,15 @@ def price_sorting_control(): #Create List of prices
     elements_count = get_element_count("//div[contains(@class,'products__item')]")
     prices = []
     for i in range(elements_count):
-        locator = "//div[contains(@class,'products__item')][{}]//span[contains(@class,'number left')]".format(i+1)
-        price_txt = get_text(locator, between="???€")
-        price_txt2 = price_txt.replace(' ','')
-        price_txt3 = String.fetch_from_left(String, price_txt2, ',')
-        price_num = float(price_txt3)
+        element_order = i+1
+        locator = f"//div[contains(@class,'products__item')][{element_order}]//span[contains(@class,'number left')]"
+        price = get_text(locator, between="???€").replace(' ','')
+        price_num = int(price.split(',')[0])
         prices.append(price_num)
 
     for i in range(elements_count-1):
-        logging.info('toto je prvy index {} a toto je druhy index {}'.format(prices[i], prices[i+1]))
-        if prices[i] < prices[i+1]:
+        index1 = i
+        index2 = i+1
+        logging.info(f'toto je prvy index {index1} a toto je druhy index {index2}')
+        if prices != sorted(prices, reverse=True):
             raise AssertionError('sorting of items is not corect form high to low')
